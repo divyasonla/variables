339f0e63439ed1132ab8781966ab91a91ff83bc7
divyasonla <163973310+divyasonla@users.noreply.github.com>
2024-04-22 10:27:46 +0530

    Add files via upload

---
+++ varibles .js
@@ -0,0 +1,80 @@
+// variable 1
+// const input =require("readline-sync")
+// let a=input.questionInt("enter a date")
+// let b=input.questionInt("enter a month")
+// let c=input.questionInt("enter a year")
+
+// {console.log(a,b ,c)}
+
+// 2
+// const input=require("readline-sync")
+// let a=input.questionInt("entre a number")
+// {console.log(a)}
+
+// 3
+
+// const input=require("readline-sync")
+// let a=input.questionInt("enter a number");
+// let b=input.questionInt("enter a number");
+// c=a;
+// a=b;
+// b=c;
+// {console.log(a)}
+// {console.log(b)}
+
+// all operation
+
+// const input=require("readline-sync")
+// let a =input.questionInt("enter a number")
+// let b=input.questionInt("enter a number")
+// s=a+b;
+// s1=a-b;
+// s2=a*b;
+// s3=Math.floor(a/b);
+// s4=a/b;
+// s5=a%b;
+// {console.log(s,"/n",s1,"/n",s2,"/n,",s3,"/n",s4,"/n",s5
+// )}
+
+// calucate remain & ques
+
+// const input=require("readline-sync")
+// let a =input.questionInt("enter a number")
+// let b =input.questionInt("enter a number")
+// s=Math.floor(a/b);
+// l=a%b;
+// {console.log(s,"/n",l)}
+
+// last digit
+
+// const input=require("readline-sync")
+// let a=input.questionInt("enter a number")
+// s=a%10;
+// {console.log(s)}
+
+// celsius
+
+// const input=require("readline-sync")
+// let c=input.questionInt("enter a number")
+// f=9*c/5+32
+// {console.log(f)}
+
+// largest no .
+
+// const input=require ("readline-sync")
+// let a= input.questionInt("enter a number")
+// let b= input.questionInt("enter a number")
+// s=Math.floor(a/b);
+// c=s*b;
+// {console.log(c)}
+
+// swap
+
+// const input=require("readline-sync")
+// let a=input.questionInt("enter a number")
+// let b=input.questionInt("enter a number")
+// a=a*b;
+// b=a/b;
+// a=a/b;
+// {console.log(a,"/n",b)}
+
